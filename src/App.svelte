83d7134db5b018fe8b576a01648454dee98e0d15
alfvar <alfvar.arvidsson@gmail.com>
2022-04-09 21:49:02 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<main>
+	<h1>Under konstruktion!</h1>
+	<p>Men <a href="https://open.spotify.com/artist/36vb4wCIqT2TjjXLEBmn0m?si=2ODV2ZMCTzWCyceETPFwLA&nd=1">här</a> kan man kolla min musik!</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: black;
+		font-size: 4em;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
