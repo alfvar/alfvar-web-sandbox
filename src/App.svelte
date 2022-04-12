021fbedae1f3d85472bcf9c7a3e23d187d74c397
alfvar <alfvar.arvidsson@gmail.com>
2022-04-13 00:06:18 +0200

    Added top nav

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,50 @@
 <main>
-	<h1 class="header">alfvar</h1>
-	<h2>En lite snällare rubrik</h2>
-	<h3>En snäll rubrik</h3>
-
-	<p>Brödtext och ett exempel på några ord på rad i en mening. Stilligt!</p>
+	<h1 class="logo">alfvar</h1>
+	<p class="subheader">is studying interaction design in Halmstad</p>
+	<div class="divider" />
+	<div class="topnav">
+		<a class="active" href="#home">UX</a>
+		<a href="#news">Artwork</a>
+		<a href="#contact">Music</a>
+		<a href="#about">About</a>
+	</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
-		background-color: none;
 	}
 
-	.header {
+	.logo {
 		font-size: 8em;
-		margin: 0.01em;
-		padding: 0em;
-		border-style: dashed;
+		margin-top: -0.4em;
+		margin-bottom: -0.2em;
 		display: flex;
 		justify-content: center;
+		padding: 0;
 	}
 
-	h2 {
-		border-style: dashed;
+	.subheader {
+		font-size: 1.2em;
+		margin: 0;
 	}
 
-	h3 {
-		border-style: dashed;
+	.divider {
+		width: 100%;
+		height: 2px;
+		background-color: #000;
+		margin: 0.75em 0;
 	}
+
+	.topnav a {
+		display: inline-block;
+		padding: 1px 16px;
+		font-size: 1.2em;
+		font-weight: 700;
+	}
+
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
