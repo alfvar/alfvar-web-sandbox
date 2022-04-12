f5fdaf0ca0b7a454beca86a7d32ec97efb0fc6cf
alfvar <alfvar.arvidsson@gmail.com>
2022-04-12 22:59:09 +0200

    I should be doing school stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,9 @@
 <main>
-	<h1>Den argaste rubriken!</h1>
+	<h1 class="header">alfvar</h1>
 	<h2>En lite snällare rubrik</h2>
 	<h3>En snäll rubrik</h3>
 
-	<p>
-		Brödtext och ett exempel på några ord på rad i en mening. Stilligt!
-	</p>
+	<p>Brödtext och ett exempel på några ord på rad i en mening. Stilligt!</p>
 </main>
 
 <style>
@@ -14,6 +12,14 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		background-color: none;
+
+	}
+
+	.header {
+		font-size: 8em;
+		margin: 0.01em;
+		padding: 0em;
 	}
 
 	@media (min-width: 640px) {
